aaf76588b18351617253b446051ee67bc7e70987
Ariel Silva Jr <ceosilvajr@gmail.com>
2016-05-21 16:04:37 +0800

    Add files via upload
    initial commit

---
+++ app/main.js
@@ -0,0 +1,67 @@
+var app=angular.module('myApp',[]);
+
+app.controller('projectController',function($scope){
+	$scope.projects = [
+		{
+			id:1,
+			position:'6u 12u$(xsmall) work-item',
+			title:'Fil-Am Radio 24/7',
+			description:'FilAm Radio is attempting to build a broadcasting network amongst the huge and fast-expanding population of Filipino-Americans in the Bay Area with an eye on linking up with other anchor stations in the West Coast and in the Pacific Northwest.',
+			image_path:'images/thumbs/filam_radio_banner.png',
+			link:'https://play.google.com/store/apps/details?id=com.filamradiousa.app.filamradio'
+		},
+		{
+			id:2,
+			position:'6u$ 12u$(xsmall) work-item',
+			title:'Pinoy Karaoke Star',
+			description:'Pinoy Karaoke Star is an app where you sing, record and share your favorite OPM (Original Pilipino Music) songs with your family and friends anywhere in the world.',
+			image_path:'images/thumbs/pinoy_karaoke_star_banner.png',
+			link:'#'
+		},
+		{
+			id:3,
+			position:'6u 12u$(xsmall) work-item',
+			title:'MyProperty.ph',
+			description:'An app for the property seller market (developers’ brokers & agents, independent licensed brokers, private individuals) which may be used for managing their inventory of property listings.',
+			image_path:'images/thumbs/my_property_banner.png',
+			link:'https://play.google.com/store/apps/details?id=ph.myproperty.android.mypropertyph'
+		},
+		{
+			id:4,
+			position:'6u$ 12u$(xsmall) work-item',
+			title:'Smart Load Services',
+			description:'Prepaid subscribers of wireless leader Smart Communications, Inc. (Smart) can now send text messages to other networks using Smart’s emergency load *SOS service by using this app.',
+			image_path:'images/thumbs/load_services_banner.png',
+			link:'#'
+		},
+		{
+			id:5,
+			position:'6u 12u$(xsmall) work-item',
+			title:'My Suki',
+			description:'It\'s a android application where you can have rewards for every purchased you made from the sari-sari store.',
+			image_path:'images/thumbs/my_suki_banner.png',
+			link:'#'
+		},
+		{
+			id:6,
+			position:'6u$ 12u$(xsmall) work-item',
+			title:'Auntie Annes Promo',
+			description:'QR Code Android application specifically designed for Auntie Annes promos.',
+			image_path:'images/thumbs/auntie_banner.png',
+			link:'#'
+		}
+	];
+});
+
+app.controller('ceosilvaController',function($scope){
+	$scope.ceo = {
+		username:'ceosilvajr',
+		name:'Ariel Q. Silva Jr.',
+		email:'ceosilvajr@gmail.com',
+		phone:'+639399080674',
+		fbLink:'https://www.facebook.com/ceosilvajr',
+		linkedInLink:'https://www.linkedin.com/in/ceosilvajr',
+		githubLink:'https://github.com/ceosilvajr',
+		twitterLink:'https://twitter.com/ceosilvajr'
+	};
+});
